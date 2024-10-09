bf87d5b8251f0632b86fcf1c0b7c6dfa4af188a5
Edgar <ediaz-gutierrez@valcom.com>
2024-10-08 22:27:33 -0400

    backup of files; first commit

---
+++ Backups/lots_steam_stable_2024_10_04/spawnpoints.lua
@@ -0,0 +1,24 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 },
+    { worldX = 36, worldY = 38, posX = 252, posY = 197, posZ = 0 },
+    { worldX = 36, worldY = 38, posX = 283, posY = 173, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 }
+  },
+  unemployed = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 }
+  }
+}
+end
---
+++ spawnpoints.lua
@@ -0,0 +1,48 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 108, posY = 224, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 149, posY = 97, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 237, posY = 31, posZ = 2 },
+    { worldX = 35, worldY = 38, posX = 138, posY = 284, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 108, posY = 224, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 149, posY = 97, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 237, posY = 31, posZ = 2 },
+    { worldX = 35, worldY = 38, posX = 138, posY = 284, posZ = 0 },
+    { worldX = 36, worldY = 38, posX = 252, posY = 197, posZ = 0 },
+    { worldX = 36, worldY = 38, posX = 283, posY = 173, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 108, posY = 224, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 149, posY = 97, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 237, posY = 31, posZ = 2 },
+    { worldX = 35, worldY = 38, posX = 138, posY = 284, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 108, posY = 224, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 149, posY = 97, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 237, posY = 31, posZ = 2 },
+    { worldX = 35, worldY = 38, posX = 138, posY = 284, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 108, posY = 224, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 149, posY = 97, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 237, posY = 31, posZ = 2 },
+    { worldX = 35, worldY = 38, posX = 138, posY = 284, posZ = 0 }
+  },
+  unemployed = {
+    { worldX = 36, worldY = 37, posX = 189, posY = 297, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 108, posY = 224, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 149, posY = 97, posZ = 0 },
+    { worldX = 35, worldY = 38, posX = 237, posY = 31, posZ = 2 },
+    { worldX = 35, worldY = 38, posX = 138, posY = 284, posZ = 0 }
+  }
+}
+end
